0ddf771e0bf2595c6c12eebbe6eab22497c22e94
erwin-marte <erwin.marte@evenbytes.com>
2025-02-28 11:41:59 +0100

    save before install cli

--- src/app/application/app-routing.module.ts
+++ src/app/application/app-routing.module.ts
@@ -2,20 +2,43 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ForbiddenComponent } from './forbidden-component/forbidden.component';
 import { NotFoundComponent } from '../webapp/not-found-component/not-found.component';
 import { LayoutComponent } from './layout/layout.component';
 
+import { DashboardComponent } from './main/dashboard/dashboard.component';
+import { SettingComponent } from './main/setting/setting.component';
+import { AssetsComponent } from './main/assets/assets.component';
+import { UserManagementComponent } from './main/user-management/user-management.component';
+
+
 const routes: Routes = [
+	{
+		path: 'auth',
+		loadChildren: () => import('./@auth/auth.module').then(m => m.AuthModule)
+	},
 	{
 		path: '',
 		component: LayoutComponent,
 		children: [
-			{
-				path: 'auth',
-				loadChildren: () => import('./@auth/auth.module').then(m => m.AuthModule)
-			},
-			{ path: '', pathMatch: 'full', redirectTo: 'auth' }
+		  	{
+				path: 'dashboard',
+				component: DashboardComponent
+		  	},
+		  	{
+				path: 'setting',
+				component: SettingComponent
+		  	},
+		  	{
+				path: 'assets',
+				component: AssetsComponent
+		  	},
+		  	{
+				path: 'user-management',
+				component: UserManagementComponent
+		  	},
+		  	{ path: '', pathMatch: 'full', redirectTo: 'dashboard' }
 		]
 	},
 	{
